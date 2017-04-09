6be175e06f8adb68b07c910871a64297c582f38d
Thembeka <thembiegeorge@gmail.com>
2017-04-09 23:30:58 +0200

    updated

--- registration_number.js
+++ registration_number.js
@@ -1,6 +1,44 @@
+var unorderedList = document.getElementById('numberplates');
 
-document.getElementById("addButton").addEventListener("click", regNum);
-function regNum() {
- var registrationNumber = document.getElementById('numberplate').value;
- document.getElementById('display').innerHTML = registrationNumber;
+function display() {
+    'use strict';
+    var plate = document.getElementById("numberplate").value;
+    if (plate.length > 0 && plate !== null) {
+        var li = document.createElement('li');
+        li.textContent = plate;
+        unorderedList.appendChild(li);
+    }
+    document.getElementById('numberplate').value = "";
+}
+
+function filter() {
+    'use strict';
+
+    var option = document.getElementById("whichTown").value;
+    var allTowns = document.getElementById("all").innerHTML;
+    var capetown = document.getElementById("capetown").innerHTML;
+    var bellville = document.getElementById("bellville").innerHTML;
+    var paarl = document.getElementById("paarl").innerHTML;
+
+    var list = document.getElementsByTagName("li");
+
+    for (var i =0; i<list.length; i++){
+
+  var currentPlate = list[i];
+  var plate = list[i].textContent.toUpperCase();
+
+  if(option === allTowns){
+
+    currentPlate.style.display = "inline-block"
+  } else if(option === capetown && plate.startsWith("CA")){
+    currentPlate.style.display = "inline-block";
+  }else if(option === bellville && plate.startsWith("CY")){
+    currentPlate.style.display = "inline-block";
+  } else if(option === paarl && plate.startsWith("CJ")){
+    currentPlate.style.display = "inline-block";
+  } else {
+    currentPlate.style.display = "none";
+  }
+}
+  document.getElementById('whichTown').value = "";
 }
